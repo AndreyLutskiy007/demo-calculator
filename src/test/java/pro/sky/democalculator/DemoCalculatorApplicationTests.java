9434ef6a8ceca9a54e0449e6fc9241ebe356cf2d
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-03-27 16:42:17 +0300

    Исходный код

---
+++ src/test/java/pro/sky/democalculator/DemoCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.democalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
